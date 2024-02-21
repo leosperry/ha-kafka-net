09ce33afc3bc22f53485180c530dab070887b528
Leonard Sperry <leonard.sperry@live.com>
2024-02-21 11:28:29 -0500

    add roku commands

---
+++ src/HaKafkaNet/Models/HaApiModels/RokuCommands.cs
@@ -0,0 +1,37 @@
+﻿namespace HaKafkaNet;
+
+/// <summary>
+/// https://www.home-assistant.io/integrations/roku/
+/// </summary>
+public enum RokuCommands
+{
+    back,
+    backspace,
+    channel_down,
+    channel_up,
+    down,
+    enter,
+    find_remote,
+    forward,
+    home,
+    info,
+    input_av1,
+    input_hdmi1,
+    input_hdmi2,
+    input_hdmi3,
+    input_hdmi4,
+    input_tuner,
+    left,
+    literal,
+    play,
+    power,
+    replay,
+    reverse,
+    right,
+    search,
+    select,
+    up,
+    volume_down,
+    volume_mute,
+    volume_up
+}
