e6f52fc779f73709978279ca96f96938a2ef54fe
Leonard Sperry <leonard.sperry@live.com>
2024-02-29 10:35:09 -0500

    Added second durable automation example

---
+++ example/HaKafkaNet.ExampleApp/Automations/ExampleDurableAutomation2.cs
@@ -0,0 +1,48 @@
+﻿
+namespace HaKafkaNet.ExampleApp;
+
+public class ExampleDurableAutomation2 : SchedulableAutomationBase
+{
+    // constants defined for code clairy only
+    const bool _shouldExecutePast = true; 
+    const bool _shouldExecuteOnError = true; 
+
+    public ExampleDurableAutomation2(IEnumerable<string> triggerIds)
+        : base(triggerIds, _shouldExecutePast, _shouldExecuteOnError)
+    {
+        // set these values apropriately
+        // https://github.com/leosperry/ha-kafka-net/wiki/Automation-Types#ischedulableautomation
+        this.IsReschedulable = false;
+
+        // https://github.com/leosperry/ha-kafka-net/wiki/Event-Timings#druable
+        this.EventTimings = EventTiming.DurableIfCached;
+    }
+
+    /// <summary>
+    /// This method replaces Continues to be true
+    /// </summary>
+    /// <param name="stateChange"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public override Task<DateTime?> CalculateNext(HaEntityStateChange stateChange, CancellationToken cancellationToken)
+    {
+        // returning null is the same as ContinuesToBeTrue returning false
+        // if you want the automation to continue, you must return a non-null value
+        // if your automation is not reschedulable, the value will be ignored
+
+        // in this example we will take an action 1 hour after an entity turns on 
+        if (stateChange.ToOnOff().New.State == OnOff.On)
+        {
+            return Task.FromResult<DateTime?>(stateChange.New.LastUpdated.AddHours(1));
+        }
+        
+        // the entity was off, cancel execution
+        return Task.FromResult<DateTime?>(default);
+    }
+
+    public override Task Execute(CancellationToken cancellationToken)
+    {
+        // you execution logic here
+        return Task.CompletedTask;
+    }
+}
