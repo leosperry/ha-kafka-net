2846c2ee58b22b54bdbd29e301e530371aa7b5d0
Leonard Sperry <leonard.sperry@live.com>
2024-04-17 07:38:36 -0400

    Added method for getting notification update type enum

--- src/HaKafkaNet/Models/HaApiModels/HaNotification.cs
+++ src/HaKafkaNet/Models/HaApiModels/HaNotification.cs
@@ -1,9 +1,34 @@
-﻿namespace HaKafkaNet;
+﻿using System.ComponentModel;
 
+namespace HaKafkaNet;
+
+/// <summary>
+/// https://www.home-assistant.io/integrations/persistent_notification/
+/// </summary>
 public record HaNotification
 {
     public string? Id { get; set; }
     public string? Title { get; set; }
     public string? Message { get; set; }
     public string? UpdateType { get; set; }
+}
+
+public static class HaNotificationExtensions
+{
+    public static HaNotificationType? GetNotificationType(this HaNotification notification)
+    {
+        if (notification.UpdateType is null)
+        {
+            return null;
+        }
+        return Enum.Parse<HaNotificationType>(notification.UpdateType, true);
+    }
+}
+
+public enum HaNotificationType
+{
+    Added,
+    Removed,
+    Updated,
+    Current
 }
